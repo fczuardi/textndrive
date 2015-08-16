7c0387c98effa2b0137b645018b725e3b646c810
Fabricio C Zuardi <fabricio@fabricio.org>
2015-08-16 17:03:53 -0300

    action to decrease credits

--- src/actions.js
+++ src/actions.js
@@ -2,6 +2,7 @@
 export const CREATE_GAME = 'CREATE_GAME';
 export const START_GAME = 'START_GAME';
 export const END_GAME = 'END_GAME';
+export const DECREASE_CREDIT = 'DECREASE_CREDIT';
 
 //action creators
 export function createGame(id) {
@@ -21,5 +22,12 @@ export function endGame(userId) {
     return {
         type: END_GAME,
         userId
+    };
+}
+
+export function decreaseCredit(playerId) {
+    return {
+        type: DECREASE_CREDIT,
+        playerId
     };
 }
